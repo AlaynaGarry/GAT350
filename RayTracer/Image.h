db1739359577b578f7baa1d52e9015c5a183e457
AlaynaGarry <Alaynagarry@gmail.com>
2021-10-21 13:27:19 -0600

    Ray Tracer

---
+++ RayTracer/Image.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "ColorBuffer.h"
+#include <string>
+
+class Image
+{
+public:
+	~Image();
+	bool Load(const std::string& filename, uint8_t alpha = 255);
+	void Flip();
+
+public: 
+	ColorBuffer colorBuffer;
+	
+private:
+	friend class Framebuffer;
+};
